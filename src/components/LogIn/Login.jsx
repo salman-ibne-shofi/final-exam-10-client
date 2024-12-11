a4964e630cb146e52178d9a5344f9014de9973ca
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-12-11 22:10:27 +0600

    created login route and register route

--- src/components/LogIn/Login.jsx
+++ src/components/LogIn/Login.jsx
@@ -1,7 +1,53 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
 	return (
-		<div>
-			<h2>this is login</h2>
+		<div className="hero bg-base-200 min-h-screen">
+			<div className="hero-content flex-col">
+				<div className="text-center lg:text-left">
+					<h1 className="text-5xl font-bold">Login now!</h1>
+				</div>
+				<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+					<form className="card-body">
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Email</span>
+							</label>
+							<input
+								type="email"
+								name="email"
+								placeholder="email"
+								className="input input-bordered"
+								required
+							/>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Password</span>
+							</label>
+							<input
+								type="password"
+								name="password"
+								placeholder="password"
+								className="input input-bordered"
+								required
+							/>
+							<div className="flex gap-2 items-center">
+								<span>New Here Please</span>
+								<Link
+									to="/register"
+									className="text-orange-600 link link-hover font-medium"
+								>
+									Register
+								</Link>
+							</div>
+						</div>
+						<div className="form-control mt-6">
+							<button className="btn btn-primary">Login</button>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,79 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
 	return (
-		<div>
-			<h2>this is register</h2>
+		<div className="hero bg-base-200 min-h-screen">
+			<div className="hero-content flex-col">
+				<div className="text-center lg:text-left">
+					<h1 className="text-5xl font-bold">Register now!</h1>
+				</div>
+				<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+					<form className="card-body">
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Name</span>
+							</label>
+							<input
+								type="name"
+								name="name"
+								placeholder="name"
+								className="input input-bordered"
+								required
+							/>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Email</span>
+							</label>
+							<input
+								type="email"
+								name="email"
+								placeholder="email"
+								className="input input-bordered"
+								required
+							/>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Photo URL</span>
+							</label>
+							<input
+								type="photo url"
+								name="photo url"
+								placeholder="photo url"
+								className="input input-bordered"
+								required
+							/>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Password</span>
+							</label>
+							<input
+								type="password"
+								name="name"
+								placeholder="password"
+								className="input input-bordered"
+								required
+							/>
+							<div className="flex gap-2 items-center">
+								<span>Already registered please</span>
+								<Link
+									to="/login"
+									className="text-blue-600 link link-hover font-medium"
+								>
+									Login
+								</Link>
+							</div>
+						</div>
+						<div className="form-control mt-6">
+							<button className="btn btn-primary">
+								Register
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
